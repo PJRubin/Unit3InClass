16e3cdbd73d9742d2b51e5870648cbfdbd0d712b
Peter Rubin <prubin@css.edu>
2020-09-24 10:35:31 -0500

    Added a sales class

---
+++ Unit3InClass/Sales.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit3InClass
+{
+    class Sales
+    {
+        private int id;
+        private String type;
+        public Sales()
+        {
+            id = 69;
+        }
+
+
+    }
+}
