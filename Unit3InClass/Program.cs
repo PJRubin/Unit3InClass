03c9846c835b3c3f123d4c8ed1e4f77c1a2c02ba
Peter Rubin <prubin@css.edu>
2020-09-24 10:31:13 -0500

    Added a PJ class

--- Unit3InClass/Program.cs
+++ Unit3InClass/Program.cs
@@ -6,7 +6,24 @@ namespace Unit3InClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); 
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodby World!");
+
+        }
+    }
+
+    class Pj
+    {
+        private int id;
+
+        public Pj()
+        {
+            id = 69;
+        }
+       
+        public void setID()
+        {
+            id = 2;
         }
     }
 }
